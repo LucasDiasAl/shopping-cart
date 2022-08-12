1c4ebf47166fa1b976be07a0eddb4a75502d7462
Lucas Dias <lucaalencarID@gmail.com>
2022-08-11 21:05:27 -0300

    requisitos 1-2 começando o projeto

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,21 @@
-const fetchProducts = () => {
-  // seu código aqui
+const item = require('../mocks/item');
+
+const fetchProducts = async (searchItem) => {
+  try {
+    if (searchItem === undefined) {
+      throw new Error('You must provide an url');
+    }
+      const dados = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${searchItem}`);
+      const refacDados = await dados.json();
+      return refacDados;
+  } catch (err) {
+    return err.message;
+  }
 };
+// const final = async () => {
+//   console.log(await fetchProducts('computador'));
+// };
+// final();
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,30 @@
 require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
+const mercadoLivreAPI = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchProducts é uma função',() => {
+    expect(typeof(fetchProducts)).toBe('function')
+  });
+
+  it('verifica se ao chamar a funçao fetchProducts com o parametro computador o fetch é chamado',async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('testa se ao chamar a funçao fetchProducts ele usa o endpoint correto ',async () => {
+    await fetchProducts('computador')
+    expect(fetch).toBeCalledWith(mercadoLivreAPI)
+  });
+
+  it('testa se a estrutura de dados retornada pela funçao fetchProducts esta correta',async () => {
+    const esperado =await fetchProducts('computador')
+    expect(esperado).toEqual(computadorSearch);
+  });
+
+  it('espera que um erro seja lançado ao chamar a funçao sem parametros',async () => {
+    expect(await fetchProducts()).toBe('You must provide an url')
+  });
+
 });
